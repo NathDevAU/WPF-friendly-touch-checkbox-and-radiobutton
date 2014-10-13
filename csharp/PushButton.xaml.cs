fea36f90e860c3344d34052d1119e8472c8f13d9
Michele Cattafesta <michelecatta@gmail.com>
2014-10-13 20:51:39 +0200

    Added PushButton control and demonstration on how to use it.
    Signed-off-by: Michele Cattafesta <michelecatta@gmail.com>

---
+++ csharp/PushButton.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleStyles
+{
+public delegate void PressedChangedEventHandler(object sender, bool IsPressed);
+    
+public partial class PushButton : Button
+{
+    public event PressedChangedEventHandler PressedChanged;
+
+    public PushButton()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnIsPressedChanged(DependencyPropertyChangedEventArgs e)
+    {
+        OnPressedChanged();
+        base.OnIsPressedChanged(e);
+    }
+
+    private void OnPressedChanged()
+    {
+        if (PressedChanged != null)
+            PressedChanged(this, this.IsPressed);
+    }
+}
+}
--- csharp/TouchFriendlyWindow.xaml.cs
+++ csharp/TouchFriendlyWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace SimpleStyles
 {
@@ -17,9 +18,44 @@ namespace SimpleStyles
     /// </summary>
     public partial class TouchFriendlyWindow : Window
     {
+        DispatcherTimer t = new DispatcherTimer(); 
+
         public TouchFriendlyWindow()
         {
             InitializeComponent();
+            t.Interval = TimeSpan.FromMilliseconds(3000);
+            t.Tick += (s, e) =>
+            {
+                MessageBox.Show("Hello world!");
+                t.Stop();
+            };
+        }
+
+                 
+
+        private void PushButton_PressedChanged(object sender, bool IsPressed)
+        {
+            cbPressedChangedEvent.IsChecked = IsPressed;
+            if (IsPressed)
+            {
+                t.Start();
+            }
+            else
+            {
+                t.Stop();
+            }
+        }
+
+        private void btnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            cbPreviewEvent.IsChecked = true;
+            t.Start();
+        }
+
+        private void btnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            cbPreviewEvent.IsChecked = false;
+            t.Stop();
         }
     }
 }
